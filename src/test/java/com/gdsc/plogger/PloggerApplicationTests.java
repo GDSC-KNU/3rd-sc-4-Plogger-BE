043de77d75a3f679402938b180f61843f79e8ade
kiryanchi <kiryanchi@naver.com>
2024-02-11 13:04:23 +0900

    INIT: Plogger

---
+++ src/test/java/com/gdsc/plogger/PloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.plogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
