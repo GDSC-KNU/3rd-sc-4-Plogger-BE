95298fe67a1ff8b1a6abeb386fc40a1941526067
Koo-EunSung <ku903@naver.com>
2024-02-17 04:11:07 +0900

    Feat: 플로깅 추가

--- src/main/java/com/gdsc/plogger/plogging/PloggingController.java
+++ src/main/java/com/gdsc/plogger/plogging/PloggingController.java
@@ -1,10 +1,13 @@
 package com.gdsc.plogger.plogging;
 
+import com.gdsc.plogger.plogging.data.dto.req.AddPloggingReq;
 import com.gdsc.plogger.plogging.data.dto.res.PloggingGetRes;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +20,10 @@ public class PloggingController {
     @GetMapping
     public ResponseEntity<List<PloggingGetRes>> getPloggings() {
         return ploggingService.getPloggings();
+    }
+
+    @PostMapping
+    public ResponseEntity<PloggingGetRes> newPlogging(@RequestBody AddPloggingReq req) {
+        return ploggingService.addPlogging(req);
     }
 }
--- src/main/java/com/gdsc/plogger/plogging/PloggingService.java
+++ src/main/java/com/gdsc/plogger/plogging/PloggingService.java
@@ -1,11 +1,13 @@
 package com.gdsc.plogger.plogging;
 
 import com.gdsc.plogger.plogging.data.Plogging;
+import com.gdsc.plogger.plogging.data.dto.req.AddPloggingReq;
 import com.gdsc.plogger.plogging.data.dto.res.PloggingGetRes;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,11 @@ public class PloggingService {
 
         return ResponseEntity.ok()
                 .body(res);
+    }
+
+    public ResponseEntity<PloggingGetRes> addPlogging(AddPloggingReq req) {
+        Plogging newPlogging = ploggingRepository.save(req.toEntity());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new PloggingGetRes(newPlogging));
     }
 }
